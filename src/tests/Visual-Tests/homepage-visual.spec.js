10c6afa987b236abda7e5868749eeb8dca24a65b
OHarris23 <oharris@realdecoy.com>
2020-03-10 16:33:11 -0500

    Added visual tests

---
+++ src/tests/Visual-Tests/homepage-visual.spec.js
@@ -0,0 +1,8 @@
+// Test script to demonstrate a visual test
+import { expect } from 'chai';
+
+// Test suite
+describe('Demo Site - Homepage - Visual UI tests', () => {
+    // Before each test, do the following
+    
+});
