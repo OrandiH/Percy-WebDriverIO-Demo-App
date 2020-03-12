2b971ed3bd1c46b972cee0406e5cadbd0e560185
OHarris23 <oharris@realdecoy.com>
2020-03-11 20:16:10 -0500

    Added settings for visual tests

--- src/tests/Visual-Tests/homepage-visual.spec.js
+++ src/tests/Visual-Tests/homepage-visual.spec.js
@@ -1,8 +1,15 @@
 // Test script to demonstrate a visual test
-import { expect } from 'chai';
+const { percySnapshot } = require('@percy/webdriverio')
 
 // Test suite
 describe('Demo Site - Homepage - Visual UI tests', () => {
     // Before each test, do the following
-    
+    beforeEach(() => {
+        browser.maximizeWindow();
+        browser.url('https://nifty-bell-cda939.netlify.com/');
+    });
+    // Test case
+    it('should load the homepage and take a screenshot', async () => {
+        await percySnapshot(browser, 'Home page screenshot');
+    });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -163,8 +163,12 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
+    before: function (capabilities, specs) {
+        // Import percySnapshot function
+        const { percySnapshot } = require('@percy/webdriverio');
+        // Make percySnapshot available as a global variable in all wdio tests
+        global.percySnapshot = percySnapshot;
+    },
     /**
      * Runs before a WebdriverIO command gets executed.
      * @param {String} commandName hook command name
