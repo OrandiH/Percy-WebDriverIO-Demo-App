a7d563f1176fc6aba0c949aefc2c33c0aee7aaf5
OHarris23 <oharris@realdecoy.com>
2020-03-13 00:44:45 -0500

    Editied index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ import styled from 'styled-components'
 import tw from "tailwind.macro"
 
 const ButtonComponent = styled.button`
-  ${tw`text-white bg-blue-900 hover:bg-blue-700 font-bold py-2 px-4 rounded-full`};
+  ${tw`text-white bg-blue-600 hover:bg-blue-700 font-bold py-2 px-4 rounded-full`};
 `
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
