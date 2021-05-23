50c192ce3203281bedc99e2f07bfed8c661abfbb
devops3210 <66638186+devops3210@users.noreply.github.com>
2021-05-23 18:49:04 +0530

    Update header.js
    changed color from White to RED

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `Red`,
             textDecoration: `none`,
           }}
         >
